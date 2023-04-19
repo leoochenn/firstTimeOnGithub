85805f94d176b7085da060aa2283bfe9f6388571
leoochenn <leoochenn@leoochenn.local>
2023-04-19 14:33:23 +0800

    remove files

--- print.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int
-main(){
-	printf("hi i am in branch ""feature/print""\n");
-	return 0;
-}
--- testbranch.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int
-main(){
-	printf("this is the newbranch\n");
-	return 0;
-}
